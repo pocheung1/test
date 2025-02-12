895bbd1b66de855ba1d2a95a7f43e625642d1834
Po Cheung <po.cheung@dominodatalab.com>
2025-02-12 11:32:45 -0500

    Add app files

---
+++ app-dash.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This is a bash script for Domino's App publishing feature
+# You can publish an app by clicking on "Publish" and selecting "App" in your
+# quick-start project.
+
+# Python/Flask example
+# export LC_ALL=C.UTF-8
+# export LANG=C.UTF-8
+# export FLASK_APP=app-flask.py
+# pip install -r requirements_apps.txt --user
+# python -m flask run --host=0.0.0.0 --port=8888
+
+# Dash example
+pip install -r requirements_apps.txt --user
+python app-dash.py
+
+# Python/HTTPServer echo app publishing example.
+# Showcases app user identity propagation.
+# pip install -r requirements_apps.txt --user
+# python app-echo.py
+
+# R/Shiny example
+# R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'
